b1bfe19a040362abc8787dfb7f20f69d5bc1c47d
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-12-10 07:26:04 +0700

    add main.go

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// custom mux
+type CustomMux struct {
+	http.ServeMux
+	middlewares []func(next http.Handler) http.Handler
+}
+
+func (c *CustomMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var current http.Handler = &c.ServeMux
+
+	for _, next := range c.middlewares {
+		current = next(current)
+	}
+
+	current.ServeHTTP(w, r)
+}
+
+func (c *CustomMux) RegisterMiddleware(middleware func(next http.Handler) http.Handler) {
+	c.middlewares = append(c.middlewares, middleware)
+}
+
+// handler
+func ShowIndex(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+// middleware
+func AllowOnlyGET(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, "", http.StatusBadRequest)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	mux := new(CustomMux)
+	mux.HandleFunc("/", ShowIndex)
+
+	var handler http.Handler = mux
+	mux.RegisterMiddleware(AllowOnlyGET)
+
+	server := new(http.Server)
+	server.Addr = ":3000"
+	server.Handler = handler
+
+	fmt.Println("server running on port 3000")
+	server.ListenAndServe()
+}
